88d5974ea5b57cabacc9fb40d9d6343fe259eb8b
sma2311 <matviychuk2311@gmail.com>
2023-12-25 20:41:12 +0100

    HW4 commit

--- package/HW4_3.java
+++ package/HW4_3.java
@@ -1,8 +1,8 @@
 public class HW4_3 {
     public static void main(String[] args) {
 
-        double ab = 10;
-        double bc = 13;
+        double ab = 20;
+        double bc = 23;
 
 
         double s = ab * bc / 2;
