4b772d6db692f160fb8ad0c96bdfd7876c725a2d
sma2311 <matviychuk2311@gmail.com>
2023-12-25 19:57:31 +0100

    HW4 commit

---
+++ package/HW4_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HW4_2 {
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Celsius : ");
+            double c = scanner.nextDouble();
+            double f = c * 9/5 + 32;
+            double k = c + 273.15;
+
+            System.out.println("Celsius: " + c);
+            System.out.println("Fahrenheit: " + f);
+            System.out.println("Kelvin: " + k);
+
+
+
+
+        }
+}
+
---
+++ package/HW4_3.java
@@ -0,0 +1,15 @@
+public class HW4_3 {
+    public static void main(String[] args) {
+
+        double ab = 10;
+        double bc = 13;
+
+
+        double s = ab * bc / 2;
+
+        System.out.println("The area is: "  + s);
+
+
+    }
+}
+
